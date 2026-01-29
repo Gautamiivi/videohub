8117f317a34da98b1f57dc2901b0362742ffd5ca
Gautam sharma <149555988+Gautamiivi@users.noreply.github.com>
2026-01-29 21:39:56 +0530

    error fix

--- videohub/backend/routes/videos.js
+++ videohub/backend/routes/videos.js
@@ -9,7 +9,9 @@ const router = express.Router();
 
 /* Needed for ES modules */
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); */
+const __dirname = path.dirname(__filename);
+
+/* MULTER SETUP */
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, "../uploads"));
